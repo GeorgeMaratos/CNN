41a2b46a1fdf62ab37edff5bb13b9cb3f17d55fc
George <gmarat19@gmail.com>
2016-11-22 06:43:58 -0500

    have the data in the right format

--- build_data.py
+++ build_data.py
@@ -4,13 +4,9 @@ import pickle
 
 print("Preparing the Data")
 animal_list = list()
-
 for i in range(12500):
  im = misc.imread("cat_scaled.{}.jpg".format(i))
- animal_list.append(im)
+ animal_list.append([np.rollaxis(np.asarray(im),2,0), np.float32(1)])
  im = misc.imread("dog_scaled.{}.jpg".format(i))
- animal_list.append(im)
-
-animal_data = np.asarray(animal_list)
-animal_data = np.rollaxis(animal_data, 3, 1)
-print(animal_data.shape)
+ animal_list.append([np.rollaxis(np.asarray(im),2,0), np.float32(1)])
+print(np.asarray(animal_list)[0][0].shape)
