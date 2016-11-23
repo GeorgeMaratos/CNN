ed9db145e816c931c509429c47a5337d205e8338
George <gmarat19@gmail.com>
2016-11-23 13:41:34 -0500

    found a way to define a function that should give output from one example, now i test it

--- test_picture.py
+++ test_picture.py
@@ -6,10 +6,10 @@ import lasagne
 def build_cnn(input_var=None): #returns a cnn: conv, max pool, two unit output
  print("Building Network...")
  #input layer
- network = lasagne.layers.InputLayer(shape=(None, 3, 128, 128), input_var=input_var)
+ input_layer = lasagne.layers.InputLayer(shape=(None, 3, 128, 128), input_var=input_var)
  #convolution and max pooling layer
  network = lasagne.layers.Conv2DLayer(
-        network, num_filters=64, filter_size=(5,5),
+        input_layer, num_filters=64, filter_size=(5,5),
         nonlinearity=lasagne.nonlinearities.rectify)
  network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
  #2 unit softmax output layer
@@ -17,14 +17,13 @@ def build_cnn(input_var=None): #returns a cnn: conv, max pool, two unit output
         lasagne.layers.dropout(network, p=.5),
         num_units=2,
         nonlinearity=lasagne.nonlinearities.softmax)
- return network
+ return input_layer, network
 
-network = build_cnn()
-input_var = T.tensor3('inputs')
+input_layer, network = build_cnn()
+input_variable = T.tensor4('inputs')
 
 with np.load('model.npz') as f:
  param_values = [f['arr_%d' % i] for i in range(len(f.files))]
  lasagne.layers.set_all_param_values(network, param_values)
 
-prediction = lasagne.layers.get_output(network)
-predict = theano.function([input_var], prediction)
+predict = theano.function([input_layer.input_var], lasagne.layers.get_output(network))
