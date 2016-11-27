acd6e0e68c2f01ff8d7492515e97be7df056b057
George <gmarat19@gmail.com>
2016-11-27 12:31:44 -0500

    changing some things

--- load_test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pickle
-
-f1 = open('animal_data.pkl', 'rb')
-final_list = pickle.load(f1)
-final_array = np.asarray(final_list)
-x_train = final_array[:2000,0]
-y_train = final_array[:2000,1]
-print(x_train.shape)
